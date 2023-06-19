14a431680e8c1f0cc8dc1a514519a6135d6d6300
Anthony Anderson <fnanderson93@gmail.com>
2023-06-19 14:52:01 -0400

    refactor: :recycle: removed comments

--- ubuntu/jammy/ipxe.sh
+++ ubuntu/jammy/ipxe.sh
@@ -10,12 +10,7 @@ set net{{ INTERFACE_ID }}/netmask 255.255.255.0
 set net{{ INTERFACE_ID }}/gateway {{ PUBLIC_GW }}
 set net{{ INTERFACE_ID }}/dns 8.8.8.8
 
-#
-# The $seed_url is used by cloud-init's nocloud-net provider to find the user-data and meta-data files. 
-# The trailing slash is important, the cloud-init process sticks 'meta-data' or 'user-data' right after, 
-# without prepending a forward slash to the file name.
 set seed_url https://raw.githubusercontent.com/anthonyra/vm-installer/main/ubuntu/jammy/templates/
-
 set vmlinuz_url https://github.com/anthonyra/vm-installer/releases/download/22.04.2/vmlinuz
 set initrd_url https://github.com/anthonyra/vm-installer/releases/download/22.04.2/initrd
 set iso_url https://releases.ubuntu.com/jammy/ubuntu-22.04.2-live-server-amd64.iso
@@ -23,8 +18,3 @@ set iso_url https://releases.ubuntu.com/jammy/ubuntu-22.04.2-live-server-amd64.i
 kernel ${vmlinuz_url} autoinstall url=${iso_url} ds=nocloud-net;s=${seed_url}
 initrd ${initrd_url}
 boot
-
-# Notes on what ip= kernel boot parameter means
-# https://www.kernel.org/doc/Documentation/filesystems/nfs/nfsroot.txt
-# The purpose of ip= in this script is to configure the hostname of the initial ubuntu installer environment, so that doesn't use the default ubuntu-server hostname.
-# If you leave the default ubuntu-server hostname, it can cause confusion at the DHCP level if you do many concurrent/parallel builds.
