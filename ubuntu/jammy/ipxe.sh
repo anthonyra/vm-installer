50bf5503c5e6f8481df49af8d4d734fd746fb957
Anthony Anderson <fnanderson93@gmail.com>
2023-06-19 15:15:44 -0400

    fix: :bug: removed https for reasons

--- ubuntu/jammy/ipxe.sh
+++ ubuntu/jammy/ipxe.sh
@@ -4,16 +4,16 @@
 # Ubuntu Installer
 #
 
-ifopen net{{ INTERFACE_ID }}
 set net{{ INTERFACE_ID }}/ip {{ PUBLIC_IP }}
 set net{{ INTERFACE_ID }}/netmask 255.255.255.0
 set net{{ INTERFACE_ID }}/gateway {{ PUBLIC_GW }}
 set net{{ INTERFACE_ID }}/dns 8.8.8.8
+ifopen net{{ INTERFACE_ID }}
 
-set seed_url https://raw.githubusercontent.com/anthonyra/vm-installer/main/ubuntu/jammy/templates/
-set vmlinuz_url https://github.com/anthonyra/vm-installer/releases/download/22.04.2/vmlinuz
-set initrd_url https://github.com/anthonyra/vm-installer/releases/download/22.04.2/initrd
-set iso_url https://releases.ubuntu.com/jammy/ubuntu-22.04.2-live-server-amd64.iso
+set seed_url http://raw.githubusercontent.com/anthonyra/vm-installer/main/ubuntu/jammy/templates/
+set vmlinuz_url http://github.com/anthonyra/vm-installer/releases/download/22.04.2/vmlinuz
+set initrd_url http://github.com/anthonyra/vm-installer/releases/download/22.04.2/initrd
+set iso_url http://releases.ubuntu.com/jammy/ubuntu-22.04.2-live-server-amd64.iso
 
 kernel ${vmlinuz_url} autoinstall url=${iso_url} ds=nocloud-net;s=${seed_url}
 initrd ${initrd_url}
