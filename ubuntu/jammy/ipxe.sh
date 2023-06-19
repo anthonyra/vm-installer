8dd82d4d5248061453229a86e02272603718a527
Anthony Anderson <fnanderson93@gmail.com>
2023-06-19 13:42:44 -0400

    feat: :sparkles: add github action to create casper artifacts

--- ubuntu/jammy/ipxe.sh
+++ ubuntu/jammy/ipxe.sh
@@ -18,8 +18,13 @@ set seed_url https://raw.githubusercontent.com/anthonyra/vm-installer/main/ubunt
 
 set vmlinuz_url https://cloud-images.ubuntu.com/jammy/current/unpacked/jammy-server-cloudimg-amd64-vmlinuz-generic
 set initrd_url https://cloud-images.ubuntu.com/jammy/current/unpacked/jammy-server-cloudimg-amd64-initrd-generic
-set iso_url https://cloud-images.ubuntu.com/releases/22.04/release/ubuntu-22.04-server-cloudimg-amd64.img
+set iso_url https://releases.ubuntu.com/jammy/ubuntu-22.04.2-live-server-amd64.iso
 
 kernel ${vmlinuz_url} autoinstall url=${iso_url} ds=nocloud-net;s=${seed_url}
 initrd ${initrd_url}
 boot
+
+# Notes on what ip= kernel boot parameter means
+# https://www.kernel.org/doc/Documentation/filesystems/nfs/nfsroot.txt
+# The purpose of ip= in this script is to configure the hostname of the initial ubuntu installer environment, so that doesn't use the default ubuntu-server hostname.
+# If you leave the default ubuntu-server hostname, it can cause confusion at the DHCP level if you do many concurrent/parallel builds.
