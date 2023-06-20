fa7fc62c1b6efd6f4fde4fe9e5f88aae5587e1a9
Anthony Anderson <fnanderson93@gmail.com>
2023-06-20 10:02:23 -0400

    refactor: :recycle: big reshuffle

--- ipxe.sh
+++ ipxe.sh
@@ -10,12 +10,12 @@ set net{{ INTERFACE_ID }}/gateway {{ PUBLIC_GW }}
 set net{{ INTERFACE_ID }}/dns 8.8.8.8
 ifopen net{{ INTERFACE_ID }}
 
-set seed_url http://raw.githubusercontent.com/anthonyra/vm-installer/main/ubuntu/jammy/templates/
+set seed_url http://raw.githubusercontent.com/anthonyra/vm-installer/main/templates/
 set vmlinuz_url http://github.com/anthonyra/vm-installer/releases/download/22.04.2/vmlinuz
 set initrd_url http://github.com/anthonyra/vm-installer/releases/download/22.04.2/initrd
 set iso_url http://github.com/anthonyra/vm-installer/releases/download/22.04.2/ubuntu-22.04.2-live-server-amd64.iso
 
-kernel ${vmlinuz_url}
-initrd ${initrd_url}
-imgargs vmlinuz initrd=initrd boot=casper ip={{ PUBLIC_IP }}/24 url=${iso_url} autoinstall ds=nocloud-net;s=${seed_url}
-boot
+kernel ${vmlinuz_url} || read void
+initrd ${initrd_url} || read void
+imgargs vmlinuz initrd=initrd boot=casper maybe-ubiquity ip={{ PUBLIC_IP }}::{{ PUBLIC_GW }}:255.255.255.0::enp1s0f0:off url=${iso_url} autoinstall ds=nocloud-net;s=${seed_url} || read void
+boot || read void
