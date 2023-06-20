48b4dc7c0639587d050ca156fa17b7bd5fd1fddc
Anthony Anderson <fnanderson93@gmail.com>
2023-06-20 10:14:30 -0400

    fix: :bug: next attempt - manually setting ip of server

--- ipxe.sh
+++ ipxe.sh
@@ -13,7 +13,7 @@ ifopen net{{ INTERFACE_ID }}
 set seed_url http://raw.githubusercontent.com/anthonyra/vm-installer/main/templates/
 set vmlinuz_url http://github.com/anthonyra/vm-installer/releases/download/22.04.2/vmlinuz
 set initrd_url http://github.com/anthonyra/vm-installer/releases/download/22.04.2/initrd
-set iso_url http://github.com/anthonyra/vm-installer/releases/download/22.04.2/ubuntu-22.04.2-live-server-amd64.iso
+set iso_url https://releases.ubuntu.com/22.04.2/ubuntu-22.04.2-live-server-amd64.iso
 
 kernel ${vmlinuz_url} || read void
 initrd ${initrd_url} || read void
