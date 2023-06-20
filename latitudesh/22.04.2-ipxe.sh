2a61b4fd03bde1af7bc908a6a0e60d2369eae493
Anthony Anderson <fnanderson93@gmail.com>
2023-06-20 11:45:08 -0400

    fix: :bug: the hunt continues

--- latitudesh/22.04.2-ipxe.sh
+++ latitudesh/22.04.2-ipxe.sh
@@ -4,16 +4,16 @@
 # Ubuntu Installer
 #
 
+ifopen net{{ INTERFACE_ID }}
 set net{{ INTERFACE_ID }}/ip {{ PUBLIC_IP }}
 set net{{ INTERFACE_ID }}/netmask 255.255.255.0
 set net{{ INTERFACE_ID }}/gateway {{ PUBLIC_GW }}
 set net{{ INTERFACE_ID }}/dns 8.8.8.8
-ifopen net{{ INTERFACE_ID }}
 
 set seed_url http://raw.githubusercontent.com/anthonyra/vm-installer/main/templates/
 set vmlinuz_url http://github.com/anthonyra/vm-installer/releases/download/22.04.2/vmlinuz
 set initrd_url http://github.com/anthonyra/vm-installer/releases/download/22.04.2/initrd
-set iso_url http://github.com/anthonyra/vm-installer/releases/download/22.04.2/ubuntu-22.04.2-live-server-amd64.iso
+set iso_url http://releases.ubuntu.com/22.04.2/ubuntu-22.04.2-live-server-amd64.iso
 
 kernel ${vmlinuz_url} || read void
 initrd ${initrd_url} || read void
