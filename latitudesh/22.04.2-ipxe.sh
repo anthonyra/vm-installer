b1ec46b8a93b6b8525da16cc6491d9940f2d268c
Anthony Anderson <fnanderson93@gmail.com>
2023-06-20 14:51:48 -0400

    fix: :ambulance: another one

--- latitudesh/22.04.2-ipxe.sh
+++ latitudesh/22.04.2-ipxe.sh
@@ -8,18 +8,15 @@ set net{{ INTERFACE_ID }}/ip {{ PUBLIC_IP }}
 set net{{ INTERFACE_ID }}/netmask 255.255.255.0
 set net{{ INTERFACE_ID }}/gateway {{ PUBLIC_GW }}
 set net{{ INTERFACE_ID }}/dns 8.8.8.8
+ifstat net{{ INTERFACE_ID }}
 
-set seed_url http://raw.githubusercontent.com/anthonyra/vm-installer/main/templates/
 set vmlinuz_url http://github.com/anthonyra/vm-installer/releases/download/22.04.2/vmlinuz
 set initrd_url http://github.com/anthonyra/vm-installer/releases/download/22.04.2/initrd
 set iso_url http://releases.ubuntu.com/22.04.2/ubuntu-22.04.2-live-server-amd64.iso
+set seed_url http://raw.githubusercontent.com/anthonyra/vm-installer/main/templates/
 
 kernel ${vmlinuz_url}
 initrd ${initrd_url}
-
-show dns
-set dns 8.8.8.8
-show dns
 
 imgargs vmlinuz \
     initrd=initrd \
@@ -27,9 +24,6 @@ imgargs vmlinuz \
     maybe-ubiquity \
     autoinstall \
     url=${iso_url} \
-    net.ifnames=0 \
-    biosdevname=0 \
-    ipv6.disable=1 \
     ip={{ PUBLIC_IP }}::{{ PUBLIC_GW }}:255.255.255.0::enp1s0f0:off \
     ds=nocloud-net;s=${seed_url}
 boot
